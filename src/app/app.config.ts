8fc3c304ce904da8d30cddb0ba2a1b4fd4c910cf
Julian <juli99nic@gmail.com>
2024-06-08 20:09:25 -0300

    Welcome screen first approach

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({ "projectId": "clinicappjs", "appId": "1:606303822612:web:5b9f95e0232e57375b1801", "storageBucket": "clinicappjs.appspot.com", "apiKey": "AIzaSyBHLd2rRgPBAho_gItxpndsMIpqGBRbDL4", "authDomain": "clinicappjs.firebaseapp.com", "messagingSenderId": "606303822612" })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WelcomeComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+];
