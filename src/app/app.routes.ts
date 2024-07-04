6a618054ba3f9cf1a2607b26f72e2f3819ef492e
Julian <juli99nic@gmail.com>
2024-07-03 22:37:02 -0300

    Added lazyload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,9 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { LayoutComponent } from './pages/layout/layout.component';
-import { UsersComponent } from './pages/users/users.component';
 import { userGuard } from './guards/admin.guard';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { CreateAppointmentComponent } from './pages/create-appointment/create-appointment.component';
-import { PatientAppointmentsComponent } from './pages/patient-appointments/patient-appointments.component';
-import { SpecialistAppointmentsComponent } from './pages/specialist-appointments/specialist-appointments.component';
-import { AllAppointmentsComponent } from './pages/all-appointments/all-appointments.component';
-import { MyPatientsComponent } from './pages/my-patients/my-patients.component';
 
 const authRoutes = [
     {
@@ -20,14 +12,52 @@ const authRoutes = [
         component: LayoutComponent,
         canActivate: [authGuard],
         children: [
-            { path: 'home', component: HomeComponent, data: { state: 'home' } },
-            { path: 'users', component: UsersComponent, canActivate: [userGuard(['admin'])], data: { state: 'users' } },
-            { path: 'profile', component: ProfileComponent, data: { state: 'profile' } },
-            { path: 'create-appointment', component: CreateAppointmentComponent, canActivate: [userGuard(['admin', 'patient'])], data: { state: 'create-appointment' } },
-            { path: 'patient-appointments', component: PatientAppointmentsComponent, canActivate: [userGuard(['patient'])], data: { state: 'patient-appointments' } },
-            { path: 'specialist-appointments', component: SpecialistAppointmentsComponent, canActivate: [userGuard(['specialist'])], data: { state: 'specialist-appointments' } },
-            { path: 'my-patients', component: MyPatientsComponent, canActivate: [userGuard(['specialist'])], data: { state: 'my-patients' } },
-            { path: 'all-appointments', component: AllAppointmentsComponent, canActivate: [userGuard(['admin'])], data: { state: 'all-appointments' } },
+            {
+                path: 'home',
+                data: { state: 'home' },
+                loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+            },
+            {
+                path: 'users',
+                canActivate: [userGuard(['admin'])],
+                data: { state: 'users' },
+                loadComponent: () => import('./pages/users/users.component').then(m => m.UsersComponent)
+            },
+            {
+                path: 'profile',
+                data: { state: 'profile' },
+                loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent)
+            },
+            {
+                path: 'create-appointment',
+                canActivate: [userGuard(['admin', 'patient'])],
+                data: { state: 'create-appointment' },
+                loadComponent: () => import('./pages/create-appointment/create-appointment.component').then(m => m.CreateAppointmentComponent)
+            },
+            {
+                path: 'patient-appointments',
+                canActivate: [userGuard(['patient'])],
+                data: { state: 'patient-appointments' },
+                loadComponent: () => import('./pages/patient-appointments/patient-appointments.component').then(m => m.PatientAppointmentsComponent)
+            },
+            {
+                path: 'specialist-appointments',
+                canActivate: [userGuard(['specialist'])],
+                data: { state: 'specialist-appointments' },
+                loadComponent: () => import('./pages/specialist-appointments/specialist-appointments.component').then(m => m.SpecialistAppointmentsComponent)
+            },
+            {
+                path: 'my-patients',
+                canActivate: [userGuard(['specialist'])],
+                data: { state: 'my-patients' },
+                loadComponent: () => import('./pages/my-patients/my-patients.component').then(m => m.MyPatientsComponent)
+            },
+            {
+                path: 'all-appointments',
+                canActivate: [userGuard(['admin'])],
+                data: { state: 'all-appointments' },
+                loadComponent: () => import('./pages/all-appointments/all-appointments.component').then(m => m.AllAppointmentsComponent)
+            },
         ]
     },
 ];
